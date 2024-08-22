2f8a12f8c73569b2c3234aff79aa34704ad4fa94
Nirupam <deynirupam12@gmail.com>
2024-08-22 20:32:30 +0530

    new files added

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log('JS connected');
+
+
+const button = document.getElementById('myButton');
+
+button.addEventListener('click', function() {
+  console.log('Button Clicked');
+
+    //alert('Button was clicked!');
+});
